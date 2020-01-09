48c8dd45a009170a3fd3802eefcf98c2cff0a872
Damian <d.pagowski@gmail.com>
2020-01-09 23:38:47 +0700

    bearer token auth updated

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 const express = require("express");
-const logger = require("morgan");
+const bodyParser = require("body-parser");
 const app = express();
 const passport = require("passport");
-const initPassportConfig = require("./passport-cfg")(
-  passport,
-  username => db.find(u => u.username == username),
-  id => db.find(u => u.id == id)
-);
+const logger = require("morgan");
+require("./passport-cfg")(passport, findByName, findById);
 
 const bcrypt = require("bcrypt");
 const flash = require("express-flash");
 const session = require("express-session");
-
+const jwt = require("jsonwebtoken");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(flash());
 app.use(
   session({
-    secret: "meo-cat",
+    secret: "secretpass",
     resave: false,
     saveUninitialized: false,
   })
@@ -27,7 +26,13 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // fake db
-const db = [];
+const db = [
+  {
+    "username": "damian",
+    "password": "$2b$10$ZbloJJ1R5Yb6t5CCcMcHOOm2ThHELxZMellJJFC9pzR4TBSM06tBm",
+    "id": 0
+  }
+];
 
 app.post("/register", checkNotAuthenticated, async (req, res, next) => {
   const password = "" + req.body.password;
@@ -46,29 +51,62 @@ app.post("/register", checkNotAuthenticated, async (req, res, next) => {
   }
 });
 
-app.post(
-  "/login",
-  [
-    checkNotAuthenticated,
-    passport.authenticate("local", { failureFlash: true }),
-  ],
-  (req, res, next) => {
-    res.json({ msg: "login-ok" });
-  }
-);
+app.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      console.log(err);
+      res.status("400").json({ errorError: info.message });
+    }
+    if (info != undefined) {
+      console.log(info.message);
+      res.status("404").json({ errorInfo: info.message });
+    } else {
+      req.logIn(user, err => {
+        const token = jwt.sign({ id: user.id }, "secretpass");
+        res.status(200).send({
+          auth: true,
+          token: token,
+          message: "user found & logged in",
+        });
+      });
+    }
+  })(req, res, next);
+});
 
 app.get("/logout", (req, res, next) => {
-  ewq.logOut();
+  req.logOut();
   res.json({ msg: "logout-ok" });
 });
 
-app.get("/", checkAuthenticated, (req, res, next) => {
-  res.json({ msg: "root-ok" });
-});
+app.get(
+  "/",
+  passport.authenticate("jwt", { failureFlash: true }),
+  (req, res, next) => {
+    res.json({ msg: "root-ok" });
+  }
+);
 
-app.get("/hello", checkAuthenticated, (req, res, next) => {
-  res.json({ msg: "hello-ok" });
-});
+app.get(
+  "/hello",
+  passport.authenticate("jwt", { session: false }),
+  (req, res, next) => {
+    res.json(req);
+  }
+);
+
+function findByName(name) {
+  console.log("finding user by name:  " + name);
+  const found = db.find(u => u.username == name);
+  console.log("Found by name: " + found);
+  return found;
+}
+
+function findById(id) {
+  console.log("finding user by id:  " + id);
+  const found = db.find(u => u.id == id);
+  console.log("Found by id: " + JSON.stringify(found));
+  return found;
+}
 
 function checkAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
--- passport-cfg.js
+++ passport-cfg.js
@@ -1,7 +1,48 @@
-const LocalStrategy = require("passport-local").Strategy;
+const passport = require("passport");
 const bcrypt = require("bcrypt");
+const LocalStrategy = require("passport-local").Strategy;
+var JwtStrategy = require("passport-jwt").Strategy;
+var ExtractJwt = require("passport-jwt").ExtractJwt;
+var optsJwt = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = "secretpass";
 
-function init(passport, getUserByName, getUserById) {
+function initPassportConfig(passport, getUserByName, getUserById) {
+  const authenticateUser = async (username, password, done) => {
+    const user = getUserByName(username);
+    console.log("username: " + username + " User found: " + user);
+    if (!user) {
+      return done(null, false, { message: "user not found" });
+    } else {
+      try {
+        const match = await bcrypt.compare(password, user.password);
+        if (match) {
+          return done(null, user);
+        } else {
+          return done(null, false, { message: "invalid password" });
+        }
+      } catch (error) {
+        return done(error);
+      }
+    }
+  };
+
+  const optsLocal = {
+    usernameField: "username",
+    passwordField: "password",
+  };
+
+  passport.use("local", new LocalStrategy(optsLocal, authenticateUser));
+
+  function awtAuthCallback(jwt_payload, done) {
+    const id = jwt_payload.id;
+    console.log("inside jwt handler - id " + id);
+    const user = getUserById(id);
+    return done(null, user);
+  }
+
+  passport.use("jwt", new JwtStrategy(optsJwt, awtAuthCallback));
+
   passport.serializeUser(function(user, done) {
     done(null, user.id);
   });
@@ -11,30 +52,5 @@ function init(passport, getUserByName, getUserById) {
       done(err, user);
     });
   });
-
-  const authenticateUser = async (username, password, done) => {
-    const user = getUserByName(username);
-    if (!user) {
-      return done(null, false, { message: "user not found" });
-    }
-
-    try {
-      const match = await bcrypt.compare(password, user.password);
-      if (match) {
-        return done(null, user);
-      } else {
-        return done(null, false, { message: "invalid password" });
-      }
-    } catch (error) {
-      return done(error);
-    }
-  };
-
-  const localStrategyConfig = {
-    usernameField: "username",
-    passwordField: "password",
-  };
-
-  passport.use(new LocalStrategy(localStrategyConfig, authenticateUser));
 }
-module.exports = init;
+module.exports = initPassportConfig;
